7249d5123edd3594829ba77554f872d28bd818c5
Durgavihashini <vihashinidurga@gmail.com>
2025-05-07 15:20:32 +0530

    Add cloudbuild.yaml and test files

---
+++ currency-function/test.py
@@ -0,0 +1,6 @@
+import unittest
+from main import currency_handler
+
+class TestCurrencyHandler(unittest.TestCase):
+    def test_dummy(self):
+        self.assertTrue(True)  # Add meaningful test later
---
+++ rate_updater/test.py
@@ -0,0 +1,6 @@
+import unittest
+from main import get_latest_rate_and_update_bq
+
+class TestRateUpdater(unittest.TestCase):
+    def test_dummy(self):
+        self.assertTrue(True)  # Add meaningful test later
